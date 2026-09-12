60dca8963f37ed1e469338b529ff813f08d06037
Gerardo González <30813716+nemis-coder@users.noreply.github.com>
2026-09-12 13:00:46 -0600

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.notBalanced = False
+        def dfs(current):
+            if current == None:
+                return 0
+                
+            left = dfs(current.left)
+            right = dfs(current.right)
+            if abs(left-right)>1:
+                self.notBalanced = True
+            return 1 + max(left,right)
+
+        dfs(root)
+        
+        if self.notBalanced:
+            return False
+        return True
+
