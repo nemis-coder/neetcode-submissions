97ca18f22e34ccce8f41a69fcc43aafa092a1d19
Gerardo González <30813716+nemis-coder@users.noreply.github.com>
2026-09-17 20:19:49 -0600

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,33 @@
+class PrefixTree:
+    END = "#"
+
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+        node[self.END] = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node:
+                return False
+            node = node[char]
+        if self.END in node:
+            return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node:
+                return False
+            node = node[char]
+        return True
+        
+        
